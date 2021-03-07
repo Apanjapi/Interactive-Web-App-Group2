ce3e895ef48216c3fac73f1dbb952419941771a7
Rocky D <duisek.kyzylorda@gmail.com>
2021-03-07 19:26:43 +0500

    commit

--- front-end/comps/article/top/index.js
+++ front-end/comps/article/top/index.js
@@ -3,6 +3,7 @@ function ArticleTopUI(){
     <div style="
       display: flex;
       flex-direction: column;
+      width: 28vw;
     ">
       <div class="top">
         <h3>Should you go vegan?</h3>
@@ -12,15 +13,17 @@ function ArticleTopUI(){
         display: flex;
         flex-direction: row;
         border: 1px solid black;
+        align-items: center;
       " class="middle">
         <div>
-          <img src="https://med-fom-surgery.sites.olt.ubc.ca/files/2014/02/ChenJ2014-273x300.jpeg" style="border-radius: 50%; height: 90px"/>
+          <img src="https://med-fom-surgery.sites.olt.ubc.ca/files/2014/02/ChenJ2014-273x300.jpeg" style="border-radius: 50%; height: 60px"/>
         </div>
         <div>
           <p style="
             color: grey;
             font-weight: bold;
             margin-left: 15px;
+            font-family: Roboto;
           ">Dr. Mike Chen</p>
         </div>
       </div>
@@ -29,6 +32,8 @@ function ArticleTopUI(){
         <p style="
           font-family: Roboto;
           font-style: italic;
+          margin-left: 2px;
+          margin-right: 2px;
         ">Published on August 26, 2021 at 05:00 am EST</p>
       </div>
     </div>
