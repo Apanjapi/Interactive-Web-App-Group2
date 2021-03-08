d5b0fde99210bd9e8e8da352608339d0846f990a
Rocky D <duisek.kyzylorda@gmail.com>
2021-03-08 23:46:54 +0500

    merged content with Junn's new page

--- front-end/comps/main-page/article-preview/index.js
+++ front-end/comps/main-page/article-preview/index.js
@@ -1,11 +1,20 @@
 function ArticlePreviewUI()
 {
     return `
-      <div style="">
-        <img src="https://placehold.it/350x150?mode=crop&height=275&widthratio=1.35" style="
-          width: 200px;
-        "/>
-      </div>
+    <div style="
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-bottom: 40px;
+  ">
+    <img src="https://sentientmedia.org/dev/wp-content/uploads/2018/10/Screen-Shot-2018-10-29-at-12.57.08-PM.jpg" style="
+      width: 250px;
+    "/>
+    <button style="
+        margin-top: 20px;
+    ">Click me!</button>
+  </div>
     `
 }
 
